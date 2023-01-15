2ebce5ec32218e1174f6354d8b0ef8bf0bfd81c5
pa-prajith <codejs100@gmail.com>
2023-01-14 22:31:58 -0800

    dwm current updated chadwm

---
+++ themes/everforest.h
@@ -0,0 +1,12 @@
+static const char black[]       = "#232A2E";
+static const char white[]       = "#D3C6AA";
+static const char gray2[]       = "#3D484D"; // unfocused window border
+static const char gray3[]       = "#475258";
+static const char gray4[]       = "#4F585E";
+static const char blue[]        = "#7FBBB3";  // focused window border
+static const char green[]       = "#A7C080";
+static const char red[]         = "#E67E80";
+static const char orange[]      = "#E69875";
+static const char yellow[]      = "#DBBC7F";
+static const char pink[]        = "#D699B6";
+static const char col_borderbar[]  = "#232A2E"; // inner border
